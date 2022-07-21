100b86ba2e13a0400f417fad81f268f3cb505d3e
Alejandro <sancha0701@gmail.com>
2022-07-20 20:14:09 -0400

    commit tree 2b8325b4aec6aa1b1d1c2a8cace9a26158e2e394

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+const superbowlWin = (records) => {
+ const winYear = records.find(record => record.result === "W"); 
+  if (winYear) {
+    return winYear.year 
+  } else  {
+    return undefined
+  }
+}
+
